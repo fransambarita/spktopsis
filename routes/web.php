9bc89ec1189b34531ad4de9cbe597a831178c2ab
fransambarita <frans.feby@student.unri.ac.id>
2019-06-09 23:16:09 +0700

    file pertama

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::group(['prefix' => 'kriteria'], function () {
+	Route::get('/', 'KriteriaController@index');
+	Route::post('/tambah', 'KriteriaController@insert');
+	Route::get('/edit/{id}', 'KriteriaController@edit');
+	Route::post('/update', 'KriteriaController@update');
+	Route::get('/hapus/{id}', 'KriteriaController@delete');
+});
+
+Route::group(['prefix' => 'alternatif'], function () {
+	Route::get('/', 'AlternatifController@index');
+	Route::get('/penilaian/{alternatif}', 'AlternatifController@value');
+	Route::post('/nilai_simpan', 'AlternatifController@store');
+	Route::get('/detail/{id}', 'AlternatifController@show');
+	Route::post('/tambah', 'AlternatifController@insert');
+	Route::get('/edit/{id}', 'AlternatifController@edit');
+	Route::post('/update', 'AlternatifController@update');
+	Route::get('/hapus/{id}', 'AlternatifController@delete');
+});
+
+Route::get('hasil', 'HasilController@index');
